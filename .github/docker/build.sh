89ecf79abc5ff33186ebee365fbcbe4124620456
devlein <104411913+devlein@users.noreply.github.com>
2023-03-31 22:04:30 +0200

    Update build.sh

--- .github/docker/build.sh
+++ .github/docker/build.sh
@@ -5,7 +5,7 @@ SCONS_FLAGS=productions=yes lto=full profile=custom.py build_feature_profile=fea
 BIN_DIR=/godot/templates
 echo key: ${KEY} - ${#KEY}
 export SCONS_CACHE=/godot/.scons-cache
-if [ ${#KEY} -ge 5 ]; then
+if [ ${#KEY} -ge 1 ]; then
   export SCRIPT_AES256_ENCRYPTION_KEY=$KEY
 fi
 
