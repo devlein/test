f1bf445346b1dc8c594f020e2b4a9e4de59d2943
devlein <104411913+devlein@users.noreply.github.com>
2023-03-31 19:23:06 +0200

    Update build.sh

--- .github/docker/build.sh
+++ .github/docker/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -l
 
-PLATFORM=$1
+ARCH=$1
 KEY=$2
-SCONS_FLAGS=productions=yes profile=custom.py build_feature_profile=feature_profile.build 
+SCONS_FLAGS=productions=yes lto=full profile=custom.py build_feature_profile=feature_profile.build 
 BIN_DIR=/godot/templates
 
 export SCONS_CACHE=/godot/.scons-cache/
-if [ ${#KEY} -ge 5 ]; then
+if [ ${#KEY} -ge 1 ]; then
   export SCRIPT_AES256_ENCRYPTION_KEY=$KEY
 fi
 
@@ -14,13 +14,13 @@ echo Building $PLATFORM
 cd /godot/source
   
 if [ "$PLATFORM" == "windows" ]; then
-  scons platform=windows arch=x86_64 lto=full use_mingw=yes ${SCONS_FLAGS} target=template_release
-  scons platform=windows arch=x86_64 lto=full use_mingw=yes ${SCONS_FLAGS} target=template_debug
+  scons platform=windows arch=${ARCH} use_mingw=yes ${SCONS_FLAGS} target=template_release
+  scons platform=windows arch=${ARCH} use_mingw=yes ${SCONS_FLAGS} target=template_debug
   
-  mv bin/godot.windows.template_release.x86_64.exe $BIN_DIR/windows_release_x86_64.exe
-  mv bin/godot.windows.template_release.x86_64.console.exe $BIN_DIR/windows_release_x86_64_console.exe
-  mv bin/godot.windows.template_debug.x86_64.exe $BIN_DIR/windows_debug_x86_64.exe
-  mv bin/godot.windows.template_debug.x86_64.console.exe $BIN_DIR/windows_debug_x86_64_console.exe
+  mv bin/godot.windows.template_release.${ARCH}.exe $BIN_DIR/windows_release_${ARCH}.exe
+  mv bin/godot.windows.template_release.${ARCH}.console.exe $BIN_DIR/windows_release_${ARCH}_console.exe
+  mv bin/godot.windows.template_debug.${ARCH}.exe $BIN_DIR/windows_debug_${ARCH}.exe
+  mv bin/godot.windows.template_debug.${ARCH}.console.exe $BIN_DIR/windows_debug_${ARCH}_console.exe
   
   strip --strip-all $BIN_DIR/windows*.exe
   
@@ -28,21 +28,21 @@ elif [ "$PLATFORM" == "linuxbsd" ]; then
   sed -i ${GODOT_SDK_LINUX_X86_64}/x86_64-godot-linux-gnu/sysroot/usr/lib/pkgconfig/dbus-1.pc -e "s@/lib@/lib64@g"
   export PATH="${GODOT_SDK_LINUX_X86_64}/bin:${BASE_PATH}"
   
-  scons platform=linuxbsd arch=x86_64 lto=full ${SCONS_FLAGS} target=template_release
-  scons platform=linuxbsd arch=x86_64 lto=full ${SCONS_FLAGS} target=template_debug
+  scons platform=linuxbsd arch=${ARCH} ${SCONS_FLAGS} target=template_release
+  scons platform=linuxbsd arch=${ARCH} ${SCONS_FLAGS} target=template_debug
   
-  mv bin/godot.linuxbsd.template_release.x86_64 $BIN_DIR/linux_release.x86_64
-  mv bin/godot.linuxbsd.template_debug.x86_64 $BIN_DIR/linux_debug.x86_64
+  mv bin/godot.linuxbsd.template_release.${ARCH} $BIN_DIR/linux_release.${ARCH}
+  mv bin/godot.linuxbsd.template_debug.${ARCH} $BIN_DIR/linux_debug.${ARCH}
   
   strip --strip-all $BIN_DIR/linux*
   
 elif [ "$PLATFORM" == "web" ]; then
   source "/emsdk/emsdk_env.sh"
 
-  scons platform=web ${SCONS_FLAGS} optimize=size target=template_release
-  scons platform=web ${SCONS_FLAGS} optimize=size target=template_release dlink_enabled=yes
-  scons platform=web ${SCONS_FLAGS} optimize=size target=template_debug
-  scons platform=web ${SCONS_FLAGS} optimize=size target=template_debug dlink_enabled=yes
+  scons platform=web optimize=size ${SCONS_FLAGS} target=template_release
+  scons platform=web optimize=size ${SCONS_FLAGS} target=template_release dlink_enabled=yes
+  scons platform=web optimize=size ${SCONS_FLAGS} target=template_debug
+  scons platform=web optimize=size ${SCONS_FLAGS} target=template_debug dlink_enabled=yes
   
   mv bin/godot.web.template_release.wasm32.zip $BIN_DIR/web_release.zip
   mv bin/godot.web.template_debug.wasm32.zip $BIN_DIR/web_debug.zip
@@ -61,15 +61,11 @@ elif [ "$PLATFORM" == "android" ]; then
   scons platform=android ${SCONS_FLAGS} target=template_debug arch=x86_64 
   
   pushd platform/android/java
-  ./gradlew generateGodotTemplates
+    ./gradlew generateGodotTemplates
   popd
   
   mv bin/android_source.zip $BIN_DIR/
   mv bin/android_debug.apk $BIN_DIR/
   mv bin/android_release.apk $BIN_DIR/
   #mv bin/godot-lib.template_release.aar $BIN_DIR/
-else
-  echo hmm
 fi
-
-ls -la /godot/templates
