36cc0e62969d129dcc2dba9dedb8941de5a98fa7
devlein <104411913+devlein@users.noreply.github.com>
2023-04-01 14:35:40 +0200

    Update build.sh

--- .github/docker/build.sh
+++ .github/docker/build.sh
@@ -31,14 +31,14 @@ if [ "$PLATFORM" == "windows" ]; then
   strip --strip-all $BIN_DIR/windows*.exe
   
 elif [ "$PLATFORM" == "linuxbsd" ]; then
-  #sed -i ${GODOT_SDK_LINUX_X86_64}/x86_64-godot-linux-gnu/sysroot/usr/lib/pkgconfig/dbus-1.pc -e "s@/lib@/lib64@g"
-  export PATH="${GODOT_SDK_LINUX_X86_64}/bin:${BASE_PATH}"
+  sed -i ${GODOT_SDK_LINUX_X86_64}/x86_64-godot-linux-gnu/sysroot/usr/lib/pkgconfig/dbus-1.pc -e "s@/lib@/lib64@g"
+  #export PATH="${GODOT_SDK_LINUX_X86_64}/bin:${BASE_PATH}"
   
   #scons platform=linuxbsd arch=x86_64 $SCONS_FLAGS target=template_release
   #scons platform=linuxbsd arch=x86_64 $SCONS_FLAGS target=template_debug
   
-  mv bin/godot.linuxbsd.template_release.x86_64 $BIN_DIR/linux_release.x86_64
-  mv bin/godot.linuxbsd.template_debug.x86_64 $BIN_DIR/linux_debug.x86_64
+  #mv bin/godot.linuxbsd.template_release.x86_64 $BIN_DIR/linux_release.x86_64
+  #mv bin/godot.linuxbsd.template_debug.x86_64 $BIN_DIR/linux_debug.x86_64
   
   export PATH="${GODOT_SDK_LINUX_X86}/bin:${BASE_PATH}"
   
